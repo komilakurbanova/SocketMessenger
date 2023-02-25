0a8d96e0f7e4ac06fc7fee47932a589faf2458e5
Ulad <anokhov04@mail.ru>
2023-02-25 15:45:25 +0300

    init base data types

---
+++ server/data.h
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class Data {
+public:
+    ~Data() = default;
+private:
+};
+
+template <class T>
+std::shared_ptr<T> As(const std::shared_ptr<Data>& obj) {
+    return std::dynamic_pointer_cast<T>(obj);
+}
+
+template <class T>
+bool Is(const std::shared_ptr<Data>& obj) {
+    return As<T>(obj) != nullptr;
+}
+
+class Message : public Data {
+public:
+    std::string_view GetContent() const {
+        return Content;
+    }
+private:
+    std::string Content;
+};
+
+class Connect : public Data {
+public:
+private:
+};
+
+class Disconnect : public Data {
+public:
+private:
+};
