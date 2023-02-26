235956c657e2fac7504feeac44cf43e411455fb9
Nikfilk2030 <Nikfilk2030@gmail.com>
2023-02-26 18:53:44 +0300

    made db_manager.h structure

--- db_manager/db_manager.h
+++ db_manager/db_manager.h
@@ -1,12 +1,25 @@
+#pragma once
+
 #include <string>
+#include <pqxx/pqxx>
+#include <stdexcept>
 
 class TDBManager {
 protected:
-    bool PathIsValid(const std::string& db_path);
+    bool PathIsValid(const std::string& db_name);
+
+    // Database connection object
+    pqxx::connection connection;
+
+    // Name of the table to manage
+    std::string table_name;
 
 public:
-    TDBManager(const std::string& db_path);
+    TDBManager(const std::string& db_name, const std::string& table_name);
 
-    void PostData();
-    void GetData();
+    // Function to post data to the database
+    void PostData(const std::string& data);
+
+    // Function to get data from the database
+    std::string GetData();
 };
