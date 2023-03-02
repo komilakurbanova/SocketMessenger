c69e266d7e3e4a8a8d9b79af8c51c346d291ade5
Komila <113191657+komilakurbanova@users.noreply.github.com>
2023-03-03 01:07:57 +0300

    generation on [0, 127]
    Диапазон ASCII представлен значениями от 0 до 127, так что, в теории, символы, сгенерированные в до 255, должны находиться в пределах диапазона ASCII, но на практике существуют некоторые символы, которые не относятся к стандарту ASCII, а являются частью других кодировок символов. 
    На некоторых платформах, в зависимости от реализации, сгенерированные значения могут находиться вне диапазона ASCII.
    Поэтому пришлось сузить.

--- hash_manager/salt_hash.cpp
+++ hash_manager/salt_hash.cpp
@@ -9,7 +9,7 @@
 std::string GenerateRandomSalt(int length) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> distrib(0, 255);
+    std::uniform_int_distribution<> distrib(0, 127);
     std::string salt;
     salt.resize(length);
     for (int i = 0; i < length; i++) {
