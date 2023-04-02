96d06fe8d386756005fb42f43003bad06ab4e9e0
Komila <113191657+komilakurbanova@users.noreply.github.com>
2023-04-02 20:41:24 +0300

    password_info instead of std::pair

--- hash_manager/test.cpp
+++ hash_manager/test.cpp
@@ -1,4 +1,4 @@
-#include "main.cpp"
+#include "salt_hash.cpp"
 
 int main() {
     // Test GenerateRandomSalt returns random salts
@@ -17,22 +17,22 @@ int main() {
 
     // Test that calling HashPassword twice with the same input password results in different salts and hashes
     std::string password = "MyPassword123";
-    auto hash_salt_1 = HashPassword(password);
-    auto hash_salt_2 = HashPassword(password);
-    assert(hash_salt_1.first != hash_salt_2.first);
-    assert(hash_salt_1.second != hash_salt_2.second);
+    PasswordInfo password_info_1 = HashPassword(password);
+    PasswordInfo password_info_2 = HashPassword(password);
+    assert(password_info_1.getHash() != password_info_2.getHash());
+    assert(password_info_1.getSalt() != password_info_2.getSalt());
 
     // Test that the output hash is different from the input password
     password = "my_password";
-    auto hash_salt = HashPassword(password);
-    assert(hash_salt.first != password);
+    PasswordInfo hash_salt = HashPassword(password);
+    assert(hash_salt.getHash() != password);
 
     // Test PasswordHashWithSalt that changing the input salt results in a different output hash
     password = "my_password";
     hash_salt = HashPassword(password);
     std::string new_salt = GenerateRandomSalt(16);
     std::string new_hash = PasswordHashWithSalt(password, new_salt);
-    assert(new_hash != hash_salt.first);
+    assert(new_hash != hash_salt.getHash());
 
     // Test HashPassword that an exception is thrown when the input password is empty
     bool exception_thrown = false;
@@ -45,24 +45,24 @@ int main() {
 
     // Test HashPassword function
     password = "MyPassword123";
-    std::pair<std::string, std::string> result = HashPassword(password);
-    std::string hash = result.first;
-    salt = result.second;
+    PasswordInfo result = HashPassword(password);
+    std::string hash = result.getHash();
+    salt = result.getSalt();
     assert(hash.length() == 64); // Check that the hash length is correct
     assert(salt.length() == 16); // Check that the salt length is correct
-    assert(CheckPassword(password, hash, salt)); // Check that the password can be checked with the hash and salt
+    assert(CheckPassword(password, result)); // Check that the password can be checked with the hash and salt
 
     // Test CheckPassword function with correct password
     password = "MyPassword123";
     hash = "7ec624e3f1f505a1d49e1f14b34b22a4382429b9e1f84ba9aae77b5f9e1bec56";
     salt = "abcdefghijklmnopqrstuvwxyz123456";
-    assert(CheckPassword(password, hash, salt)); // Check that the correct password is accepted
+    assert(CheckPassword(password, PasswordInfo(hash, salt))); // Check that the correct password is accepted
 
     // Test CheckPassword function with incorrect password
     password = "WrongPassword";
     hash = "7ec624e3f1f505a1d49e1f14b34b22a4382429b9e1f84ba9aae77b5f9e1bec56";
     salt = "abcdefghijklmnopqrstuvwxyz123456";
-    assert(!CheckPassword(password, hash, salt)); // Check that the incorrect password is rejected
+    assert(!CheckPassword(password, PasswordInfo(hash, salt))); // Check that the incorrect password is rejected
 
     std::cout << "All tests passed!" << std::endl;
     return 0;
