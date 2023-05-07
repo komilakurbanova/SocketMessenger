d83cd30a73a97859dec00470c982a182d373e2c4
Nikfilk2030 <Nikfilk2030@gmail.com>
2023-05-07 15:23:49 +0300

    little refactoring for GUI. added readme and separated part of main into function

--- console_GUI/gui.cpp
+++ console_GUI/gui.cpp
@@ -1,11 +1,9 @@
+#include <cstring>
+#include <iostream>
 #include <ncurses.h>
+#include <random>
 #include <string>
 #include <vector>
-#include <iostream>
-#include <random>
-
-// запускать g++ -std=c++11  gui.cpp -o gui.out -lncurses
-// установить sudo apt-get install libncurses5-dev libncursesw5-dev
 
 enum {
     ESC = -2,
@@ -22,8 +20,7 @@ int choose_one_of_list(int max_rows, int max_cols, std::vector<std::string> &lis
     while (true) {
         clear();
         for (int i = choice; i < std::min(choice + num_rows, static_cast<int>(list_to_show.size())); i++) {
-            if (i == choice) // текущий совпадает с выбором пользователя
-            {
+            if (i == choice) { // текущий совпадает с выбором пользователя
                 attron(COLOR_PAIR(2));
                 addch('>'); // выводим указатель
                 printw("%s\n", list_to_show[i].c_str());
@@ -39,14 +36,16 @@ int choose_one_of_list(int max_rows, int max_cols, std::vector<std::string> &lis
                 endwin();
                 return ESC;
             case KEY_BACK:
-                return BACK; // вернуться, но пока не понимаю, как запоминать предущий этап
+                return BACK; // вернуться, но пока не понимаю, как запоминать предыдущий этап
             case KEY_UP:
-                if (choice) // указатель вверх
+                if (choice) { // указатель вверх
                     --choice;
+                }
                 break;
             case KEY_DOWN:
-                if (choice < list_to_show.size() - 1) // указатель вниз
+                if (choice < list_to_show.size() - 1) { // указатель вниз
                     ++choice;
+                }
                 break;
             case '\n':
                 return choice; // какой индекс из списка контактов выбрал
@@ -54,26 +53,31 @@ int choose_one_of_list(int max_rows, int max_cols, std::vector<std::string> &lis
     }
 }
 
-int main() {
+std::vector<std::string> generate_contact_names() {
     std::vector<std::string> contacts;
-  // генерация списка контактов
     std::vector<std::string> first_names = {"Alice", "Bob", "Charlie", "David", "Emily", "Frank", "Grace", "Hannah", "Isaac", "Jack", "Kate", "Luke", "Megan", "Nathan", "Olivia", "Peter", "Quinn", "Rachel", "Sarah", "Tom", "Ursula", "Victoria", "Wendy", "Xander", "Yvette", "Zachary"};
     std::vector<std::string> last_names = {"Adams", "Brown", "Clark", "Davis", "Evans", "Franklin", "Garcia", "Hernandez", "Irwin", "Jackson", "Kim", "Lee", "Martin", "Nguyen", "Owens", "Patel", "Quinn", "Rodriguez", "Smith", "Taylor", "Upton", "Vargas", "Walker", "Xu", "Young", "Zhang"};
     std::random_device rd;
     std::mt19937 rng(rd());
     std::uniform_int_distribution<int> first_name_dist(0, first_names.size() - 1);
     std::uniform_int_distribution<int> last_name_dist(0, last_names.size() - 1);
-    for (int i = 0; i < 100; ++i) {
+    for (int i = 0; i < 100; ++i) { // почему 100?
         std::string full_name = first_names[first_name_dist(rng)] + " " + last_names[last_name_dist(rng)];
         contacts.push_back(full_name);
     }
+    return contacts;
+}
+
+int main(int argc, const char *argv[]) {
+    auto contacts = generate_contact_names();
 
     initscr();
     start_color();
     init_pair(1, COLOR_WHITE, COLOR_BLUE); // Цвет окна
     curs_set(0);
     keypad(stdscr, true);
-    int max_rows, max_cols;
+    int max_rows;
+    int max_cols;
     getmaxyx(stdscr, max_rows, max_cols);
 
     int return_value = choose_one_of_list(max_rows, max_cols, contacts);
